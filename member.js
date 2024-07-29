8e401aafe54f63770c1928217cc2b90a95743a6b
askgiridhari <162861468+askgiridhari@users.noreply.github.com>
2024-07-29 16:11:05 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,52 @@
+
+function skillsMember() {
+    var skills = [
+        {
+            name: "HTML",
+            progress: 85
+        },
+        {
+            name: "CSS",
+            progress: 75
+        },
+        {
+            name: "Javascript",
+            progress: 60
+        },
+        {
+            name: "Node.js",
+            progress: 50
+        },
+        {
+            name: "React",
+            progress: 40
+        },
+        {
+            name: "Angular",
+            progress: 35
+        },
+        {
+            name: "MongoDB",
+            progress: 30
+        },
+        {
+            name: "Express",
+            progress: 25
+        },
+        {
+            name: "Python",
+            progress: 20
+        },
+        {
+            name: "Java",
+            progress: 15
+        },
+        {
+            name: "C#",
+            progress: 10
+        }
+    ];
+
+    return skills;
+}
+
